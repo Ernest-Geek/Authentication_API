b64b82ccd61ca530d60d0dc5657950e0c2ddb759
Emmanuel Kisanda <103537166+Kisanda25@users.noreply.github.com>
2024-06-07 13:54:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from app import create_app
 
+# Create an instance of the Flask application using the factory function
 app = create_app()
 
+# Entry point for running the Flask application
 if __name__ == "__main__":
+    # Run the Flask application in debug mode
     app.run(debug=True)
